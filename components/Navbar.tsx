43f7fa81467fcb69c467fa1949b7e79236d2e5b3
ErmoGarcia <ermiutx@gmail.com>
2020-12-10 22:44:51 +0100

    feat: navbar improved

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/Navbar.module.scss'
 export default function Navbar() {
     return (
         <nav className={styles.navigation}>
-            <h1 className={styles.logo}>Mus Online</h1>
+            <div className={styles.logo}>
+                <img src="/logo.svg" alt="logo" />
+            </div>
             <ul className={styles.routes}>
                 <li className={styles.link}>Login</li>
                 <li className={styles.link}>Register</li>
