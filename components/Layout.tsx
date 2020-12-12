2a626e5c448a174cdcfebb065c0ab72a89841f16
ErmoGarcia <ermiutx@gmail.com>
2020-12-12 17:11:39 +0100

    feat: common layout added

---
+++ components/Layout.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+import styles from 'styles/Layout.module.scss'
+import Head from 'next/head'
+import Navbar from 'components/Navbar'
+
+export default function Layout({children}) {
+    const [theme, setTheme] = useState();
+
+    useEffect(() => {
+        if(!theme){
+            setTheme(localStorage.getItem('theme') || 'light')
+        }
+
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    return (
+        <div className={`${styles.container} ${theme}`}>
+            <Head>
+                <title>Mus Online</title>
+                <link rel="icon" href="/logo.svg" />
+            </Head>
+
+            <Navbar setTheme={setTheme} theme={theme}></Navbar>
+
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,15 @@
-import Head from 'next/head'
 import styles from 'styles/Home.module.scss'
-import Navbar from 'components/Navbar'
-
-import { useState, useEffect } from 'react'
+import Layout from 'components/Layout'
 
 export default function Home() {
-    const [theme, setTheme] = useState();
-
-    useEffect(() => {
-        if(!theme){
-            setTheme(localStorage.getItem('theme') || 'light')
-        }
-
-        localStorage.setItem('theme', theme);
-    }, [theme]);
-
     return (
-        <div className={theme}>
-          <Head>
-            <title>Mus Online</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-
-          <Navbar setTheme={setTheme} theme={theme}></Navbar>
-
-          <main className={styles.main}>
-              <div>
-                  <h1 className={styles.title}>Welcome to Mus Online</h1>
-                  <h3 className={styles.author}>
-                      By <a href="https://github.com/ErmoGarcia">Ermo</a>
-                  </h3>
-              </div>
-          </main>
-        </div>
+        <Layout>
+            <div className={styles.container}>
+              <h1 className={styles.title}>Welcome to Mus Online</h1>
+              <h3 className={styles.author}>
+                  by <a href="https://github.com/ErmoGarcia">Ermo</a>
+              </h3>
+            </div>
+        </Layout>
     )
 }
