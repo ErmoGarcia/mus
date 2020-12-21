bf678ed38d606331fe90f3aa87beed5dd14eed1e
ErmoGarcia <ermiutx@gmail.com>
2020-12-21 20:22:00 +0100

    feat: login form with animation when focused

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,25 @@
+import styles from 'styles/Login.module.scss'
 import Layout from 'components/Layout'
 
 export default function Login() {
     return(
         <Layout>
-            <div>
-                <h1>Login</h1>
+            <div className={styles.container}>
+                <form action="" method="post">
+                    <div className={styles.field}>
+                        <input type="text" name="username" required />
+                        <label htmlFor="username">
+                            <span className={styles.fieldname}>Username</span>
+                        </label>
+                    </div>
+
+                    <div className={styles.field}>
+                        <input type="text" name="password" required />
+                        <label htmlFor="password">
+                            <span className={styles.fieldname}>Password</span>
+                        </label>
+                    </div>
+                </form>
             </div>
         </Layout>
     )
