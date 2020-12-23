7b3ffbfa011111210248b96e83fb900bd34b70a0
ErmoGarcia <ermiutx@gmail.com>
2020-12-23 11:17:27 +0100

    feat: different layouts for mobile devices

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,27 +5,33 @@ import FormField from 'components/FormField'
 export default function Login() {
     return(
         <Layout>
-            <div className={styles.container}>
-                <form action="" method="post">
-                    <div className={styles.row}>
+            <form action="" method="post">
+                <div className={styles.container}>
+                    <div className={styles.item}>
                         <FormField fieldname="username"></FormField>
                     </div>
 
-                    <div className={styles.row}>
+                    <div className={styles.item}>
                         <FormField fieldname="password"></FormField>
                     </div>
 
-                    <div className={styles.row}>
-                        <button className={styles.submit} type="submit">Login</button>
+                    <div className={styles.item}>
+                        <button className={styles.btn} type="submit">
+                            Login
+                        </button>
                     </div>
 
-                    <div className={styles.row}>
-                        <div className={styles.separator}>
-                            <span> or </span>
-                        </div>
+                    <div className={styles.item+" "+styles.separator}>
+                        <span> or </span>
                     </div>
-                </form>
-            </div>
+
+                    <div className={styles.item}>
+                        <button className={styles.btn} type="submit">
+                            Sign in with Google
+                        </button>
+                    </div>
+                </div>
+            </form>
         </Layout>
     )
 }
